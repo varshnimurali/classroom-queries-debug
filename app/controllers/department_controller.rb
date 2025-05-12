becb496ff1a9b896322001fb7a4bcdd74751f40c
varshnimurali <vmurali0@chicagobooth.edu>
2025-05-12 19:42:29 +0000

    save changes

--- app/controllers/department_controller.rb
+++ app/controllers/department_controller.rb
@@ -1,4 +1,4 @@
-class DepartmentsController < ApplicationController
+class DepartmentController < ApplicationController
   def index
     @departments = Department.all.order({ :created_at => :desc })
 
@@ -7,7 +7,8 @@ class DepartmentsController < ApplicationController
 
   def show
     the_id = params.fetch("path_id")
-    @department = Department.where({:id => the_id })
+    @department = Department.find(the_id)
+
 
     render({ :template => "departments/show" })
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,15 @@ Rails.application.routes.draw do
 
   get("/departments", { :controller => "department", :action => "index" })
   
-  get("/departments/:path_id", { :controller => "departments", :action => "show" })
+  get("/departments/:path_id", { :controller => "department", :action => "show" })
 
-  post("/insert_department", { :controller => "departments", :action => "create" })
+  post("/insert_department", { :controller => "department", :action => "create" })
 
-  get("/departments", { :controller => "departments", :action => "index" })
+  get("/departments", { :controller => "department", :action => "index" })
   
-  post("/modify_department/:path_id", { :controller => "departments", :action => "update" })
+  post("/modify_department/:path_id", { :controller => "department", :action => "update" })
 
-  get("/delete_department/:path_id", { :controller => "departments", :action => "destroy" })
+  get("/delete_department/:path_id", { :controller => "department", :action => "destroy" })
 
   #------------------------------
   # Course routes
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   
   get("/courses/:path_id", { :controller => "courses", :action => "show" })
 
-  get("/insert_course", { :controller => "courses", :action => "create" })
+  post("/insert_course", { :controller => "courses", :action => "create" })
 
   get("/courses", { :controller => "courses_controller", :action => "index" })
   
